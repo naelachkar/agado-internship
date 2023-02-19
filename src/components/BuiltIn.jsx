d073bfb43b327576d192a8ffe14874c8a4fa64ce
naelachkar <111461079+naelachkar@users.noreply.github.com>
2023-02-19 16:52:11 +0200

    deleting unused dependency and fixing loading bug

--- src/components/BuiltIn.jsx
+++ src/components/BuiltIn.jsx
@@ -2,17 +2,35 @@ import { useEffect, useState } from "react";
 
 export default function BuiltIn() {
   const [text, setText] = useState();
-  const voices = window.speechSynthesis.getVoices();
   const [voiceList, setVoiceList] = useState();
   const [voiceIndex, setVoiceIndex] = useState(0);
 
+  // getVoice is async
+  let timeout = 0;
+  const maxTimeout = 2000;
+  const interval = 250;
+  const loadVoices = (cb) => {
+    const voices = speechSynthesis.getVoices();
+    if (voices.length > 0) {
+      return cb(undefined, voices);
+    }
+    if (timeout >= maxTimeout) {
+      return cb(new Error("loadVoices max timeout exceeded"));
+    }
+    timeout += interval;
+    setTimeout(() => loadVoices(cb), interval);
+  };
+
   useEffect(() => {
-    setVoiceList(voices);
+    loadVoices((err, voices) => {
+      if (err) return console.error(err);
+      setVoiceList(voices);
+    });
   }, []);
 
   function textToSpeech() {
     const msg = new SpeechSynthesisUtterance(text);
-    msg.voice = voices[voiceIndex];
+    msg.voice = voiceList[voiceIndex];
     window.speechSynthesis.speak(msg);
   }
 
@@ -23,14 +41,14 @@ export default function BuiltIn() {
         <textarea onChange={(e) => setText(e.target.value)}></textarea>
         <button onClick={textToSpeech}>Speak</button>
       </div>
-      {voiceList.length > 0 && (
+      {voiceList?.length > 0 && (
         <select onChange={(e) => setVoiceIndex(e.target.value)}>
           {voiceList.map((voice, index) => (
             <option key={voice.voiceURI} value={index}>
               {voice.name}
             </option>
           ))}
-        </select>   
+        </select>
       )}
     </>
   );
