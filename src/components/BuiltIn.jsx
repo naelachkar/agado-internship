e6452ea2a116a7fc55d39fd376080c650a46a401
naelachkar <111461079+naelachkar@users.noreply.github.com>
2023-02-19 16:34:46 +0200

    drop down menu

--- src/components/BuiltIn.jsx
+++ src/components/BuiltIn.jsx
@@ -1,29 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function BuiltIn() {
-    const [text, setText] = useState();
-    const [lang, setLang] = useState("en-US");
+  const [text, setText] = useState();
+  const voices = window.speechSynthesis.getVoices();
+  const [voiceList, setVoiceList] = useState();
+  const [voiceIndex, setVoiceIndex] = useState(0);
 
-    function textToSpeech() {
-        const voices = window.speechSynthesis.getVoices();
-        // console.log(voices);
-        const msg = new SpeechSynthesisUtterance(text);
-        msg.lang = lang;
-        window.speechSynthesis.speak(msg);
-    }
+  useEffect(() => {
+    setVoiceList(voices);
+  }, []);
 
-    return (
-        <>
-            <h1>Built-in JavaScript</h1>
-            <div>
-                <textarea onChange={(e) => setText(e.target.value)}></textarea>
-                <button onClick={textToSpeech}>Speak in {lang}</button>
-            </div>
-            <div>
-                <button onClick={() => setLang("en-US")}>English (US)</button>
-                <button onClick={() => setLang("en-GB")}>English (UK)</button>
-                <button onClick={() => setLang("he-IL")}>Hebrew</button>
-            </div>
-        </>
-    );
+  function textToSpeech() {
+    const msg = new SpeechSynthesisUtterance(text);
+    msg.voice = voices[voiceIndex];
+    window.speechSynthesis.speak(msg);
+  }
+
+  return (
+    <>
+      <h1>Built-in JavaScript</h1>
+      <div>
+        <textarea onChange={(e) => setText(e.target.value)}></textarea>
+        <button onClick={textToSpeech}>Speak</button>
+      </div>
+      {voiceList.length > 0 && (
+        <select onChange={(e) => setVoiceIndex(e.target.value)}>
+          {voiceList.map((voice, index) => (
+            <option key={voice.voiceURI} value={index}>
+              {voice.name}
+            </option>
+          ))}
+        </select>   
+      )}
+    </>
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     const navigate = useNavigate();
   return (
     <>
-      <h1>Text to speech</h1>
+      <h1>Text-to-speech methods</h1>
       <button onClick={() => navigate("/builtIn")}>Built-in JavaScript</button>
     </>
   );
