5fdf02f93b3b21363549f93b33ccdc9862ae1210
Naël Achkar <naelachkar@hotmail.fr>
2023-02-21 14:52:58 +0200

    Library demo installed

--- src/components/RSR.jsx
+++ src/components/RSR.jsx
@@ -1,7 +1,25 @@
-
+import regeneratorRuntime from "regenerator-runtime";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 export default function RSR() {
-    return (
-        <h1>React Speech Recognition</h1>
-    )
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
 }
