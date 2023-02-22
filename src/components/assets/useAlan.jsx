b4d3f955791a94d2de0e8f7939adbf8b05247a68
naelachkar <111461079+naelachkar@users.noreply.github.com>
2023-02-22 15:20:46 +0200

    Working on useAlan hook

--- src/components/assets/useAlan.jsx
+++ src/components/assets/useAlan.jsx
@@ -1,8 +1,24 @@
 import alanBtn from "@alan-ai/alan-sdk-web";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+const COMMANDS = {
+  OPEN_BOX: "open-box",
+};
 
 export default function useAlan() {
   const [alanInstance, setAlanInstance] = useState();
+
+  const openBox = useCallback(() => {
+    alanInstance.playText("Opening the box");
+  }, [alanInstance]);
+
+  useEffect(() => {
+    window.addEventListener(COMMANDS.OPEN_BOX, openBox);
+
+    return () => {
+      window.removeEventListener(COMMANDS.OPEN_BOX, openBox);
+    };
+  }, [openBox]);
 
   useEffect(() => {
     if (alanInstance != null) return;
@@ -11,8 +27,8 @@ export default function useAlan() {
         top: "20px",
         left: "20px",
         key: import.meta.env.VITE_ALAN_KEY,
-        onCommand: (commandData) => {
-          console.log(commandData);
+        onCommand: ({ command }) => {
+          window.dispatchEvent(new CustomEvent(command));
         },
       })
     );
