6fd9360112a28eb77c3ef8d0c1f0659a399ec28a
naelachkar <111461079+naelachkar@users.noreply.github.com>
2023-02-22 13:22:06 +0200

    Preparing the box

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import "./Alan.css";
+
 export default function Alan() {
+
+    const [bool, setBool] = useState(true);
+
+
   return (
     <>
       <h1>Alan Demo</h1>
+      <button onClick={() => setBool(!bool)}>Open the Box</button>
+      <div className="test" hidden={bool}>
+        <span>I'm open 👋</span>
+      </div>
     </>
   );
 }
