5eaf95d86ea4c4faf6968b7c683ba12e691c8ecc
Naël Achkar <naelachkar@hotmail.fr>
2023-02-23 14:07:53 +0200

    Alan script done

--- src/AlanScript/Alan_Demo.js
+++ src/AlanScript/Alan_Demo.js
@@ -16,18 +16,3 @@ intent(`Change (the|) background (color|) to ${COLORS}`, p => {
 intent("react", p => {
     p.play({command: "react"})
 })
-
-// // Give Alan some knowledge about the world
-// corpus(`
-//     Hello, I'm Alan.
-//     This is a demo application.
-//     You can learn how to teach Alan useful skills.
-//     I can teach you how to write Alan Scripts.
-//     I can help you. I can do a lot of things. I can answer questions. I can do tasks.
-//     But they should be relevant to this application.
-//     I can help with this application.
-//     I'm Alan. I'm a virtual assistant. I'm here to help you with applications.
-//     This is a demo script. It shows how to use Alan.
-//     You can create dialogs and teach me.
-//     For example: I can help navigate this application.
-// `);
