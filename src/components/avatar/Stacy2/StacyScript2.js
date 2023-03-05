dcb51ed397b25c911360811b41ecb96ea4958dd9
Naël Achkar <naelachkar@hotmail.fr>
2023-03-05 18:02:10 +0200

    NW

--- src/components/avatar/Stacy2/StacyScript2.js
+++ src/components/avatar/Stacy2/StacyScript2.js
@@ -1,6 +1,141 @@
 import img from "../../assets/AgadoStacy.jpeg";
 
 function StacyScript() {
+  const mediaPipe = [
+    [
+      0.5184473991394043, 0.19857102632522583, -0.4661916494369507,
+      0.9999380111694336,
+    ],
+    [
+      0.5129076838493347, 0.18722839653491974, -0.433306485414505,
+      0.9999089241027832,
+    ],
+    [
+      0.5160479545593262, 0.18674170970916748, -0.4338514804840088,
+      0.9998816251754761,
+    ],
+    [
+      0.5187481641769409, 0.1860240399837494, -0.433749794960022,
+      0.9999138116836548,
+    ],
+    [
+      0.5020149350166321, 0.1879521608352661, -0.4711328446865082,
+      0.999954342842102,
+    ],
+    [
+      0.49476301670074463, 0.1880979686975479, -0.47156670689582825,
+      0.9999581575393677,
+    ],
+    [
+      0.4866214096546173, 0.18882864713668823, -0.47178855538368225,
+      0.9999587535858154,
+    ],
+    [
+      0.5034785866737366, 0.1925061047077179, -0.22678159177303314,
+      0.9999306201934814,
+    ],
+    [
+      0.46174517273902893, 0.19603635370731354, -0.40305137634277344,
+      0.999884843826294,
+    ],
+    [
+      0.5219654440879822, 0.2075759321451187, -0.3825068175792694,
+      0.9999147653579712,
+    ],
+    [
+      0.505943238735199, 0.2088380753993988, -0.4364081919193268,
+      0.9999150037765503,
+    ],
+    [
+      0.550936222076416, 0.2546997368335724, -0.037486519664525986,
+      0.9999656677246094,
+    ],
+    [
+      0.4064693748950958, 0.26170068979263306, -0.39366310834884644,
+      0.999937891960144,
+    ],
+    [
+      0.6147176027297974, 0.31728506088256836, -0.15687943994998932,
+      0.613289475440979,
+    ],
+    [
+      0.45581722259521484, 0.33261364698410034, -0.669360339641571,
+      0.9783005118370056,
+    ],
+    [
+      0.6129634976387024, 0.2444310486316681, -0.5151222348213196,
+      0.9155669212341309,
+    ],
+    [
+      0.4670288562774658, 0.24943692982196808, -0.949566662311554,
+      0.9925957322120667,
+    ],
+    [
+      0.6087474226951599, 0.2294623851776123, -0.5653623342514038,
+      0.8893728256225586,
+    ],
+    [
+      0.46332675218582153, 0.23852841556072235, -1.0400984287261963,
+      0.9895573258399963,
+    ],
+    [
+      0.597891628742218, 0.22339902818202972, -0.5696360468864441,
+      0.8766529560089111,
+    ],
+    [
+      0.44876545667648315, 0.23436687886714935, -1.0303035974502563,
+      0.9878892302513123,
+    ],
+    [
+      0.5918992161750793, 0.23101332783699036, -0.5246537923812866,
+      0.8134378790855408,
+    ],
+    [
+      0.45318689942359924, 0.23831532895565033, -0.9543798565864563,
+      0.9708070755004883,
+    ],
+    [
+      0.5382571220397949, 0.40107491612434387, 0.12368778139352798,
+      0.9999783039093018,
+    ],
+    [
+      0.4532555937767029, 0.40129175782203674, -0.12379671633243561,
+      0.9999792575836182,
+    ],
+    [
+      0.5417776703834534, 0.49615994095802307, 0.4205757975578308,
+      0.28017207980155945,
+    ],
+    [
+      0.4591883718967438, 0.5050889253616333, -0.149933323264122,
+      0.9285887479782104,
+    ],
+    [
+      0.5417945981025696, 0.5702221989631653, 0.8147954344749451,
+      0.7250357270240784,
+    ],
+    [
+      0.4254809021949768, 0.599043607711792, 0.035987891256809235,
+      0.9696704149246216,
+    ],
+    [
+      0.5224378108978271, 0.5871374011039734, 0.8441605567932129,
+      0.714364767074585,
+    ],
+    [
+      0.40907198190689087, 0.6120690107345581, 0.0473012812435627,
+      0.7977349162101746,
+    ],
+    [
+      0.6137090921401978, 0.5972755551338196, 0.7303290367126465,
+      0.8330100774765015,
+    ],
+    [
+      0.46534353494644165, 0.6402043104171753, -0.13935737311840057,
+      0.9576383233070374,
+    ],
+  ];
+
   // Set our main variables
   let scene,
     renderer,
@@ -8,10 +143,21 @@ function StacyScript() {
     model, // Our character
     neck, // Reference to the neck bone in the skeleton
     waist, // Reference to the waist bone in the skeleton
+    head,
+    rightShoulder,
     leftShoulder,
+    rightArm,
     leftArm,
+    rightForeArm,
     leftForeArm,
+    rightHand,
     leftHand,
+    rightUpLeg,
+    leftUpLeg,
+    rightLeg,
+    leftLeg,
+    rightFoot,
+    leftFoot,
     mixer, // THREE.js animations mixer
     clock = new THREE.Clock(); // Used for anims, which run to a clock instead of frame rate
 
@@ -68,7 +214,7 @@ function StacyScript() {
 
         model.traverse((o) => {
           //To get the list of all the bones
-          console.log(o);
+          console.log(o.name);
 
           if (o.isMesh) {
             o.castShadow = true;
@@ -77,35 +223,121 @@ function StacyScript() {
           }
 
           // Reference the neck and waist bones
+          if (o.isBone && o.name === "mixamorigHead") {
+            head = o;
+          }
           if (o.isBone && o.name === "mixamorigNeck") {
             neck = o;
           }
           if (o.isBone && o.name === "mixamorigSpine") {
             waist = o;
           }
+          if (o.isBone && o.name === "mixamorigRightShoulder") {
+            rightShoulder = o;
+          }
           if (o.isBone && o.name === "mixamorigLeftShoulder") {
             leftShoulder = o;
+          }
+          if (o.isBone && o.name === "mixamorigRightArm") {
+            rightArm = o;
           }
           if (o.isBone && o.name === "mixamorigLeftArm") {
             leftArm = o;
           }
+          if (o.isBone && o.name === "mixamorigRightForeArm") {
+            rightForeArm = o;
+          }
           if (o.isBone && o.name === "mixamorigLeftForeArm") {
             leftForeArm = o;
+          }
+          if (o.isBone && o.name === "mixamorigRightHand") {
+            rightHand = o;
           }
           if (o.isBone && o.name === "mixamorigLeftHand") {
             leftHand = o;
           }
+          if (o.isBone && o.name === "mixamorigRightUpLeg") {
+            rightUpLeg = o;
+          }
+          if (o.isBone && o.name === "mixamorigLeftUpLeg") {
+            leftUpLeg = o;
+          }
+          if (o.isBone && o.name === "mixamorigRightLeg") {
+            rightLeg = o;
+          }
+          if (o.isBone && o.name === "mixamorigLeftLeg") {
+            leftLeg = o;
+          }
+          if (o.isBone && o.name === "mixamorigRightFoot") {
+            rightFoot = o;
+          }
+          if (o.isBone && o.name === "mixamorigLeftFoot") {
+            leftFoot = o;
+          }
         });
 
-        // Set the models initial scale
-        model.scale.set(7, 7, 7);
+        // Set the models initial scale (its size)
+        model.scale.set(12, 12, 12);
         model.position.y = -11;
+
+        // Other methods available:
+        // add(object) - Adds a child object to the current object.
+        // remove(object) - Removes a child object from the current object.
+        // getPosition() - Returns the current position of the object.
+        // setPosition(x, y, z) - Sets the position of the object to the specified coordinates.
+        // translateX(distance) - Translates the object along the x-axis by the specified distance.
+        // translateY(distance) - Translates the object along the y-axis by the specified distance.
+        // translateZ(distance) - Translates the object along the z-axis by the specified distance.
+        // rotateX(angle) - Rotates the object around the x-axis by the specified angle.
+        // rotateY(angle) - Rotates the object around the y-axis by the specified angle.
+        // rotateZ(angle) - Rotates the object around the z-axis by the specified angle.
+        // scale.set(x, y, z) - Sets the scale of the object to the specified values in each direction.
 
         // waist.rotation.y = 45;
         // neck.rotation.y = 45;
-        // leftArm.rotation.x = -45;
-        // leftForeArm.rotation.y = -45;
+        leftArm.rotation.x = -45;
+        leftForeArm.rotation.y = -45;
         // console.log(leftArm)
+
+        head.position.x = mediaPipe[0][0];
+        head.position.y = mediaPipe[0][1];
+        head.position.z = mediaPipe[0][2];
+        rightShoulder.position.x = mediaPipe[11][0];
+        rightShoulder.position.y = mediaPipe[11][1];
+        rightShoulder.position.z = mediaPipe[11][2];
+        leftShoulder.position.x = mediaPipe[12][0];
+        leftShoulder.position.y = mediaPipe[12][1];
+        leftShoulder.position.z = mediaPipe[12][2];
+        rightArm.position.x = mediaPipe[13][0];
+        rightArm.position.y = mediaPipe[13][1];
+        rightArm.position.z = mediaPipe[13][2];
+        leftArm.position.x = mediaPipe[14][0];
+        leftArm.position.y = mediaPipe[14][1];
+        leftArm.position.z = mediaPipe[14][2];
+        rightForeArm.position.x = mediaPipe[15][0];
+        rightForeArm.position.y = mediaPipe[15][1];
+        rightForeArm.position.z = mediaPipe[15][2];
+        leftForeArm.position.x = mediaPipe[16][0];
+        leftForeArm.position.y = mediaPipe[16][1];
+        leftForeArm.position.z = mediaPipe[16][2];
+        rightUpLeg.position.x = mediaPipe[23][0];
+        rightUpLeg.position.y = mediaPipe[23][1];
+        rightUpLeg.position.z = mediaPipe[23][2];
+        leftUpLeg.position.x = mediaPipe[24][0];
+        leftUpLeg.position.y = mediaPipe[24][1];
+        leftUpLeg.position.z = mediaPipe[24][2];
+        rightLeg.position.x = mediaPipe[25][0];
+        rightLeg.position.y = mediaPipe[25][1];
+        rightLeg.position.z = mediaPipe[25][2];
+        leftLeg.position.x = mediaPipe[26][0];
+        leftLeg.position.y = mediaPipe[26][1];
+        leftLeg.position.z = mediaPipe[26][2];
+        rightFoot.position.x = mediaPipe[27][0];
+        rightFoot.position.y = mediaPipe[27][1];
+        rightFoot.position.z = mediaPipe[27][2];
+        leftFoot.position.x = mediaPipe[28][0];
+        leftFoot.position.y = mediaPipe[28][1];
+        leftFoot.position.z = mediaPipe[28][2];
 
         scene.add(model);
       },
